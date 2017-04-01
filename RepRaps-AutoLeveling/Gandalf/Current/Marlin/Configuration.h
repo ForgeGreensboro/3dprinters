2c7ecd2130fdc6f904ae979e1b8e0ef09b1f55e4
NCBob <nc4rg@yandex.com>
2017-03-31 21:20:29 -0400

    Updated configuration for Gandalf

--- RepRaps-AutoLeveling/Gandalf/Current/Marlin/Configuration.h
+++ RepRaps-AutoLeveling/Gandalf/Current/Marlin/Configuration.h
@@ -86,9 +86,7 @@
 // User-specified version info of this build to display in [Pronterface, etc] terminal window during
 // startup. Implementation of an idea by Prof Braino to inform user that any changes made to this
 // build by the user have been successfully uploaded into firmware.
-//#define STRING_CONFIG_H_AUTHOR "(Gandalf, Bed Leveling and Cooling Fan)" // Who made the changes.
-#define STRING_CONFIG_H_AUTHOR "(Betty, Bed Leveling and Cooling Fan)" // Who made the changes.
-//#define STRING_CONFIG_H_AUTHOR "(Eric, Bed Leveling and Cooling Fan)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Gandalf, Bed Leveling and Cooling Fan)" // Who made the changes.
 #define SHOW_BOOTSCREEN
 #define STRING_SPLASH_LINE1 SHORT_BUILD_VERSION // will be shown during bootup in line 1
 #define STRING_SPLASH_LINE2 WEBSITE_URL         // will be shown during bootup in line 2
@@ -138,8 +136,6 @@
 // Optional custom name for your RepStrap or other custom machine
 // Displayed in the LCD "Ready" message
 #define CUSTOM_MACHINE_NAME "Gandalf"
-//#define CUSTOM_MACHINE_NAME "Betty"
-//#define CUSTOM_MACHINE_NAME "Eric"
 
 // Define this to set a unique identifier for this printer, (Used by some programs to differentiate between machines)
 // You can use an online service to generate a random UUID. (eg http://www.uuidgenerator.net/version4)
@@ -490,7 +486,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 380, 100 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 441.86046511627907, 100 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -584,7 +580,7 @@
 //  (0,0)
 #define X_PROBE_OFFSET_FROM_EXTRUDER -40  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 0  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -3.36   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -2.2   // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 8000
